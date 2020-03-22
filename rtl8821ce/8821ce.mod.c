d33706fb2299defc4e13712cbad9f998e8c58968
Tobias Sarnowski <tobias@sarnowski.io>
2020-03-22 13:56:21 +0100

    Added tpm2-tools and bluetooth mesh capabilities

---
+++ rtl8821ce/8821ce.mod.c
@@ -0,0 +1,31 @@
+#include <linux/build-salt.h>
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(.gnu.linkonce.this_module) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+MODULE_INFO(depends, "");
+
+MODULE_ALIAS("pci:v000010ECd0000C821sv*sd*bc*sc*i*");
+MODULE_ALIAS("pci:v000010ECd0000C82Asv*sd*bc*sc*i*");
+MODULE_ALIAS("pci:v000010ECd0000C82Bsv*sd*bc*sc*i*");
+
+MODULE_INFO(srcversion, "C541F8151CF7DC9F68A3ABA");
